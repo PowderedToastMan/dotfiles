30ddabae5523986d3708dd1ea6dbee66121b2e18
Dave Kil <dave@thekilempire.com>
2022-09-27 14:48:02 -0400

    Fix install.ps1

--- install.ps1
+++ install.ps1
@@ -4,8 +4,11 @@ git submodule update
 New-Item -ErrorAction Ignore -Type Directory `
   -Path $ENV:LOCALAPPDATA/nvim
 New-Item -ErrorAction Ignore -Type SymbolicLink `
-  -Path $ENV:LOCALAPPDATA/nvim/init.vim `
-  -Value $ENV:USERPROFILE\dotfiles\nvim\init.vim
+  -Path $ENV:LOCALAPPDATA/nvim/init.lua `
+  -Value $ENV:USERPROFILE\dotfiles\nvim\init.lua
 New-Item -ErrorAction Ignore -Type SymbolicLink `
   -Path $ENV:LOCALAPPDATA/nvim/ginit.vim `
   -Value $ENV:USERPROFILE\dotfiles\nvim\ginit.vim
+New-Item -ErrorAction Ignore -Type SymbolicLink `
+  -Path $ENV:LOCALAPPDATA/nvim/lua `
+  -Value $ENV:USERPROFILE\dotfiles\nvim\lua
