858d6a558befbc049cea7ebf60e3ef2525638d39
Dave <dave@thekilempire.com>
2022-09-28 14:39:25 -0400

    Minor syntax changes

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,9 @@
-function ls --wraps='exa --group-directories-first' --description 'alias ls exa --group-directories-first'
-  exa --group-directories-first $argv; 
+if which exa >/dev/null 2>&1
+  function ls --wraps='exa --group-directories-first' --description 'alias ls exa --group-directories-first'
+    exa --group-directories-first $argv;
+  end
+else
+  function ls --wraps='ls --color=auto' --description 'alias ls ls --color=auto'
+   command ls --color=auto $argv;
+  end
 end
--- fish/functions/vim.fish
+++ fish/functions/vim.fish
@@ -1,3 +1,11 @@
-function vim --wraps=nvim --description 'alias vim nvim'
-  nvim $argv; 
+if which nvim >/dev/null 2>&1
+  function vim --wraps=nvim --description 'alias vim nvim'
+    nvim $argv;
+  end
+  function vi --wraps=nvim --description 'alias vi nvim'
+    nvim $argv;
+  end
+  function vimdiff --wraps='nvim -d' --description 'alias vimdiff nvim -d'
+    nvim -d $argv;
+  end
 end
