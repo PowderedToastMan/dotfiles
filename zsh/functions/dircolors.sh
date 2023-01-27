f163c2f00025b94706528c39cf3ada06cf06b988
Dave <dave@thekilempire.com>
2023-01-27 10:34:36 -0500

    Refactor dircolors

--- zsh/functions/dircolors.sh
+++ zsh/functions/dircolors.sh
@@ -1,2 +1,9 @@
 # Set ls colors
 eval $(dircolors ~/.dotfiles/dircolors/solarized/dircolors.256dark)
+
+# Additional dircolors
+export LS_COLORS="${LS_COLORS}*.zst=00;38;5;61:"
+export LS_COLORS="${LS_COLORS}*.iso=01;38;5;61:"
+
+# Set background color
+printf %b '\e]11;#1d2021\a'
